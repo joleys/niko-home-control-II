34a4fbe4bbde1278a4f9b822aa73d0143151a195
Filip Van Ham <tux@imac-boven.local>
2020-11-30 00:38:20 +0100

    allow adding coco manually

--- custom_components/nhc2/config_flow.py
+++ custom_components/nhc2/config_flow.py
@@ -8,7 +8,7 @@ from homeassistant.const import CONF_HOST, CONF_USERNAME, \
 from nhc2_coco.coco_discover_profiles import CoCoDiscoverProfiles
 from nhc2_coco.coco_login_validation import CoCoLoginValidation
 
-from .const import DOMAIN, CONF_SWITCHES_AS_LIGHTS
+from .const import DOMAIN, CONF_SWITCHES_AS_LIGHTS, KEY_MANUAL
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -62,7 +62,7 @@ class Nhc2FlowHandler(config_entries.ConfigFlow):
                 return await self._show_user_config_form()
 
             return self.async_create_entry(
-                title=user_name + ' (' + self._selected_coco[1].replace(':', '') + ')',
+                title=user_name + ' (' + host + ')',
                 data={
                     CONF_HOST: host,
                     CONF_ADDRESS: self._selected_coco[1],
@@ -84,24 +84,36 @@ class Nhc2FlowHandler(config_entries.ConfigFlow):
                     'Type': 'hobby'
                 })
 
-        found_coco_count = len(self._all_cocos) if self._all_cocos else 0
-        # If there is only one controller, we continue to the next step
-        if found_coco_count == 1:
-            self._selected_coco = self._all_cocos[0]
-            return await self._show_user_config_form()
-
-        if found_coco_count > 1:
-            return await self._show_host_config_form()
-
-        return self.async_abort(reason="no_controller_found")
+        return await self._show_host_config_form()
 
     async def async_step_host(self, user_input=None):
         self._errors = {}
-        _LOGGER.debug(self.hass.config_entries.async_entries(DOMAIN))
-
-        self._selected_coco = \
-            list(filter(lambda x: x[0] == user_input[CONF_HOST] or x[3] == user_input[CONF_HOST], self._all_cocos))[0]
+        if user_input[CONF_HOST] == KEY_MANUAL:
+            return await self._show_manual_host_config_form()
+        else:
+            self._selected_coco = \
+                list(filter(lambda x: x[0] == user_input[CONF_HOST] or x[3] == user_input[CONF_HOST], self._all_cocos))[0]
         return await self._show_user_config_form()
+
+    async def async_step_manual_host(self, user_input=None):
+        self._errors = {}
+
+        disc = CoCoDiscoverProfiles(user_input[CONF_HOST])
+        self._all_cocos = await disc.get_all_profiles()
+        if self._all_cocos is not None and len(self._all_cocos) == 1:
+            self._selected_coco = self._all_cocos[0]
+            _LOGGER.debug(str(self._all_cocos))
+            for coco in self._all_cocos:
+                if coco[2] is not None:
+                    coco[2].insert(0, {
+                        'Uuid': 'hobby',
+                        'Name': 'hobby',
+                        'Type': 'hobby'
+                    })
+
+            return await self._show_user_config_form()
+        else:
+            return self.async_abort(reason="no_controller_found")
 
     async def _show_host_config_form(self):
         """Show the configuration form to edit NHC2 data."""
@@ -112,6 +124,9 @@ class Nhc2FlowHandler(config_entries.ConfigFlow):
             host_listing[dkey] = [x[3] + ' (' + x[0] + ')']
             if i == 0:
                 first = dkey
+        host_listing[KEY_MANUAL] = 'Manual Input'
+        if first is None:
+            first = KEY_MANUAL
         return self.async_show_form(
             step_id='host',
             errors=self._errors,
@@ -134,12 +149,21 @@ class Nhc2FlowHandler(config_entries.ConfigFlow):
             step_id='user',
             errors=self._errors,
             description_placeholders={
-                "host": self._selected_coco[3] if self._selected_coco[3] is not None else self._selected_coco[0],
-                "address": self._selected_coco[1]
+                "host": self._selected_coco[3] if self._selected_coco[3] is not None else self._selected_coco[0]
             },
             data_schema=vol.Schema({
                 vol.Required(CONF_USERNAME, default=first): vol.In(profile_listing),
                 vol.Required(CONF_PASSWORD, default=None): str,
                 vol.Optional(CONF_SWITCHES_AS_LIGHTS, default=False): bool
+            }),
+        )
+
+    async def _show_manual_host_config_form(self):
+        """Show the configuration form to edit NHC2 data."""
+        return self.async_show_form(
+            step_id='manual_host',
+            errors=self._errors,
+            data_schema=vol.Schema({
+                vol.Required(CONF_HOST, default=None): str
             }),
         )
