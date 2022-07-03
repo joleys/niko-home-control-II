2d11640ff18261671c9be4ecea9ead7a97b2b4e1
Jo Leys <jo.leys@gmail.com>
2022-07-03 19:39:01 +0200

    add generic routines (@nielsvissers)

--- custom_components/nhc2/switch.py
+++ custom_components/nhc2/switch.py
@@ -5,6 +5,7 @@ from homeassistant.components.switch import SwitchEntity
 from .helpers import nhc2_entity_processor
 from .nhccoco.coco import CoCo
 from .nhccoco.coco_switch import CoCoSwitch
+from .nhccoco.coco_generic import CoCoGeneric
 from .nhccoco.coco_device_class import CoCoDeviceClass
 
 from .const import DOMAIN, KEY_GATEWAY, BRAND, SWITCH
@@ -27,6 +28,99 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
                               KEY_ENTITY,
                               lambda x: NHC2HassSwitch(x))
     )
+    gateway.get_devices(CoCoDeviceClass.GENERIC,
+         nhc2_entity_processor(hass,
+                               config_entry,
+                               async_add_entities,
+                               KEY_ENTITY,
+                               lambda x: NHC2HassGeneric(x))
+     )
+     
+class NHC2HassGeneric(SwitchEntity):
+    """Representation of an NHC2 Switch."""
+
+    def __init__(self, nhc2switch: CoCoGeneric, optimistic=True):
+        """Initialize a switch."""
+        _LOGGER.debug("init Switch")
+        self._nhc2switch = nhc2switch
+        self._optimistic = optimistic
+        self._is_on = nhc2switch.is_on
+        nhc2switch.on_change = self._on_change
+
+    def _on_change(self):
+        self._is_on = self._nhc2switch.is_on
+        self.schedule_update_ha_state()
+
+    def turn_off(self, **kwargs) -> None:
+        """Pass - not in use."""
+        pass
+
+    def turn_on(self, **kwargs) -> None:
+        """Pass - not in use."""
+        pass
+
+    async def async_turn_on(self, **kwargs):
+        """Instruct the switch to turn on."""
+        self._nhc2switch.turn_on()
+        if self._optimistic:
+            self._is_on = True
+            self.schedule_update_ha_state()
+
+    async def async_turn_off(self, **kwargs):
+        """Instruct the switch to turn off."""
+        self._nhc2switch.turn_off()
+        if self._optimistic:
+            self._is_on = False
+            self.schedule_update_ha_state()
+
+    def nhc2_update(self, nhc2switch: CoCoSwitch):
+        """Update the NHC2 switch with a new object."""
+        self._nhc2switch = nhc2switch
+        nhc2switch.on_change = self._on_change
+        self.schedule_update_ha_state()
+
+    @property
+    def unique_id(self):
+        """Return the lights UUID."""
+        return self._nhc2switch.uuid
+
+    @property
+    def uuid(self):
+        """Return the lights UUID."""
+        return self._nhc2switch.uuid
+
+    @property
+    def should_poll(self):
+        """Return false, since the light will push state."""
+        return False
+
+    @property
+    def name(self):
+        """Return the lights name."""
+        return self._nhc2switch.name
+
+    @property
+    def available(self):
+        """Return true if the light is online."""
+        return self._nhc2switch.online
+
+    @property
+    def is_on(self):
+        """Return true if the light is on."""
+        return self._is_on
+
+    @property
+    def device_info(self):
+        """Return the device info."""
+        return {
+            'identifiers': {
+                (DOMAIN, self.unique_id)
+            },
+            'name': self.name,
+            'manufacturer': BRAND,
+            'model': SWITCH,
+            'via_hub': (DOMAIN, self._nhc2switch.profile_creation_id),
+        }
 
 
 class NHC2HassSwitch(SwitchEntity):
