2fbdb41551752c0fdd99146a66377224637d2aad
joleys <jo.leys@gmail.com>
2023-02-04 15:54:51 +0100

    Update fan_action_fan.py

--- custom_components/nhc2/entities/fan_action_fan.py
+++ custom_components/nhc2/entities/fan_action_fan.py
@@ -19,10 +19,11 @@ class Nhc2FanActionFanEntity(FanEntity):
         self._device.after_change_callbacks.append(self.on_change)
 
         self._attr_available = self._device.is_online
-        self._attr_unique_id = device_instance.uuid
+        self._attr_unique_id = self._device.uuid
         self._attr_should_poll = False
 
-        self._attr_supported_features = FanEntityFeature.SET_SPEED, FanEntityFeature.PRESET_MODE
+        self._attr_supported_features = FanEntityFeature.SET_SPEED | FanEntityFeature.PRESET_MODE
+        self._preset_modes = self._device.possible_fan_speeds
 
     @property
     def device_info(self):
@@ -43,17 +44,26 @@ class Nhc2FanActionFanEntity(FanEntity):
 
     @property
     def preset_modes(self) -> str:
-        return self._device.possible_fan_speeds
+        return self._preset_modes
 
     @property
     def percentage(self) -> int:
         """Return the current speed percentage."""
-        return ordered_list_item_to_percentage(self.preset_modes, self._device.fan_speed)
+        return ordered_list_item_to_percentage(self._preset_modes, self._device.fan_speed)
 
     @property
     def speed_count(self) -> int:
         """Return the number of speeds the fan supports."""
-        return len(self.preset_modes)
+        return len(self._preset_modes)
+    
+    @property
+    def supported_features(self):
+        """Return supported features."""
+        return self._attr_supported_features
+
+    def set_percentage(self, percentage: int) -> None:
+        """Set the speed percentage of the fan."""
+        self._device.set_fan_speed(self._gateway, percentage_to_ordered_list_item(self._preset_modes, percentage))
 
     def on_change(self):
         self.schedule_update_ha_state()
@@ -63,5 +73,5 @@ class Nhc2FanActionFanEntity(FanEntity):
         self.on_change()
 
     async def async_set_percentage(self, percentage: int) -> None:
-        self._device.set_fan_speed(self._gateway, percentage_to_ordered_list_item(self.preset_modes, percentage))
+        self._device.set_fan_speed(self._gateway, percentage_to_ordered_list_item(self._preset_modes, percentage))
         self.on_change()
