979b058a25b968518da4a66e93943495e2a1f5ac
Tijs Verkoyen <tijs@verkoyen.eu>
2023-02-07 16:44:46 +0100

    Only add the Nhc2GenericDomestichotwaterunitProgramEntity Select entity if there are programs.

--- custom_components/nhc2/select.py
+++ custom_components/nhc2/select.py
@@ -28,8 +28,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     if len(device_instances) > 0:
         entities = []
         for device_instance in device_instances:
-            entities.append(
-                Nhc2GenericDomestichotwaterunitProgramEntity(device_instance, hub, gateway)
-            )
+            if device_instance.possible_programs:
+                entities.append(Nhc2GenericDomestichotwaterunitProgramEntity(device_instance, hub, gateway))
 
         async_add_entities(entities)
