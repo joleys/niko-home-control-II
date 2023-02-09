e9d7c6440fbb280212590ad58fcc0581b6b70aa0
Tijs Verkoyen <tijs@verkoyen.eu>
2023-02-09 19:40:23 +0100

    Remove Camera entity from the NHC Access Control as this is not the correct device

--- custom_components/nhc2/camera.py
+++ custom_components/nhc2/camera.py
@@ -5,9 +5,6 @@ from homeassistant.const import CONF_USERNAME
 
 from .nhccoco.coco import CoCo
 
-from .entities.accesscontrol_action_camera import Nhc2AccesscontrolActionCameraEntity
-from .nhccoco.devices.accesscontrol_action import CocoAccesscontrolAction
-
 from .const import DOMAIN, KEY_GATEWAY
 
 KEY_ENTITY = 'nhc2_cameras'
@@ -22,12 +19,3 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 
     gateway: CoCo = hass.data[KEY_GATEWAY][config_entry.entry_id]
     hub = (DOMAIN, config_entry.data[CONF_USERNAME])
-
-    device_instances = gateway.get_device_instances(CocoAccesscontrolAction)
-    _LOGGER.info('→ Found %s NHC Access Control Actions', len(device_instances))
-    if len(device_instances) > 0:
-        entities = []
-        for device_instance in device_instances:
-            entities.append(Nhc2AccesscontrolActionCameraEntity(device_instance, hub, gateway))
-
-        async_add_entities(entities)
